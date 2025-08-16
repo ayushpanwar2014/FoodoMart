9cc35848a891088267f63c4d8382538a3b984e4e
ayushpanwar2014 <ayush.panwar2014@gmail.com>
2025-08-16 09:36:25 +0530

    +added loading spinner

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,27 +1,33 @@
 /* eslint-disable react/prop-types */
 import { useContext } from "react"
 import "./FoodDisplay.css"
-import {StoreContext} from "../../Context/StoreContext"
+import { StoreContext } from "../../Context/StoreContext"
 import FoodItem from "../FoodItem/FoodItem"
 
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
 
-    const {food_list} = useContext(StoreContext)
-    
+  const { food_list } = useContext(StoreContext)
+
   return (
     <div className="food_display" id="food_display">
       <h2>Top Dishes Near You</h2>
-      <div className="food-display-list">
-        {
-            food_list.filter((item) => category === "All" ? true : item.category === category).map( (item,index) => {
 
+      {
+        !food_list && food_list.length === 0 ? (
+          <div className="btn-spinner">
+            <div className="spinner"></div>
+          </div>
+        ) : (
 
-                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-
-
-            })
-        }
-      </div>
+          <div className="food-display-list">
+            {
+              food_list.filter((item) => category === "All" ? true : item.category === category).map((item, index) => {
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+              })
+            }
+          </div>
+        )
+      }
     </div>
   )
 }
